3361300d039fe6a531f8c37e95cbfbc1a287f29a
Andrew Neitsch <andrew@neitsch.ca>
2008-04-20 22:45:56 -0600

    Add history, remove variable binding leaks into the real module.

--- .python/readline.py
+++ .python/readline.py
@@ -12,35 +12,50 @@ You'd think that you could just put 'import rlcompleter' in
 sitecustomize.py, but when readline is initialized it'll `tput smm` :/
 """
 
-import imp
-import sys
-from os.path import abspath, dirname
+def __readline_shim_setup():
+    import atexit
+    import imp
+    import os
+    import sys
+    from os.path import abspath, dirname
 
-def same_directory(path1, path2):
-    path1 = abspath(dirname(path1))
-    path2 = abspath(dirname(path2))
-    return path1 == path2
+    def same_directory(path1, path2):
+        path1 = abspath(dirname(path1))
+        path2 = abspath(dirname(path2))
+        return path1 == path2
 
-def import_next(name, filename):
-    """Import the next module 'name' in the search path that's not in the
-    same directory as 'filename'."""
+    def import_next(name, filename):
+        """Import the next module 'name' in the search path that's not in the
+        same directory as 'filename'."""
 
-    paths = sys.path
+        paths = sys.path
 
-    mod_dir = dirname(filename)
-    paths = filter(lambda x: x != mod_dir, sys.path)
+        mod_dir = dirname(filename)
+        paths = filter(lambda x: x != mod_dir, sys.path)
 
-    while paths:
-        real_module = imp.find_module(name, paths)
-        new_filename = real_module[1]
-        if same_directory(filename, new_filename):
-            # In order not to fall into an infinite loop, drop the first
-            # entry of path and try again.
-            paths.pop(0)
-        else:
-            imp.load_module(name, *real_module)
-            return
+        while paths:
+            real_module = imp.find_module(name, paths)
+            new_filename = real_module[1]
+            if same_directory(filename, new_filename):
+                # In order not to fall into an infinite loop, drop the first
+                # entry of path and try again.
+                paths.pop(0)
+            else:
+                imp.load_module(name, *real_module)
+                return
 
-import_next(__name__, __file__)
+    import_next(__name__, __file__)
 
-import rlcompleter
+    import rlcompleter
+
+    # While we're here, enable persistent history
+    readline = sys.modules['readline']
+    HIST_FILE = os.path.join(os.getenv('HOME'), '.pyhist')
+    if os.path.isfile(HIST_FILE):
+        readline.read_history_file(HIST_FILE)
+    atexit.register(lambda: readline.write_history_file(HIST_FILE))
+
+# Bindings in this module leak through to the real readline, so we create
+# only one with an unlikely name and remove it after.
+__readline_shim_setup()
+del __readline_shim_setup
