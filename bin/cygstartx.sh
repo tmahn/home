330d2b23a08da2df5d97386c22384b312857187d
Andrew Neitsch <andrew@neitsch.ca>
2008-06-09 13:20:07 -0600

    Make it work without c:\cygwin in global PATH.

--- bin/cygstartx.sh
+++ bin/cygstartx.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-/usr/X11R6/bin/run Xwin_GL \
-    -multiwindow -clipboard -emulate3buttons -unixkill
 
-/usr/bin/sleep 5
+set -eu
+
+PATH="/bin:/usr/X11R6/bin:${PATH}"
+
+run /usr/X11R6/bin/Xwin_GL \
+    -multiwindow -emulate3buttons -unixkill \
+    -screen 0 2432x1064 &
+
+MAX_CHECKS=50
+CHECK=1
+while ! [ -s "/tmp/.X11-unix/X0" ] \
+    &&[ "${CHECK}" -lt "${MAX_CHECKS}" ]
+do
+    (( CHECK++ ))
+    sleep 0.1
+done
+
 export DISPLAY=:0
 
-/usr/X11R6/bin/xset +fp /home/neitsch/.fonts
-/usr/X11R6/bin/xrdb /home/neitsch/.Xresources
+FONT_DIR=~/.fonts
+if [ -d "${FONT_DIR}" ]; then
+    xset +fp "${FONT_DIR}"
+fi
+xrdb ~/.Xresources
