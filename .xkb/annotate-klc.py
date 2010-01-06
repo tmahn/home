ab6a0c3013175fe816c4901ae88eb73caca4ae1f
Andrew Neitsch <andrew@neitsch.ca>
2010-01-06 06:12:12 -0700

    Not all of the dead keys were working properly in Windows. The best I could find was a binary windows keyboard layouts from 2008-12-19, which doesn’t appear to correspond to any checked-in .klc version. I used MSKLC to convert that binary layout to source and extend it slightly, with heavy manual testing. Let’s hope that checking in the binary layouts with the source will lead to fewer incidents like that in the future, and allow easier setup of new machines.

--- .xkb/annotate-klc.py
+++ .xkb/annotate-klc.py
@@ -6,7 +6,7 @@ import re
 import sys
 import unicodedata
 
-deadkey_line = re.compile('[0-9A-Fa-f]{4}\s+[0-9A-Fa-f]{4}\s+//')
+deadkey_line = re.compile('[0-9A-Fa-f]{4}\s+([0-9A-Fa-f]{4})\s+// . -> .')
 
 def rewrite(filename):
     contents = open(filename).read().decode('UTF-16')
@@ -14,7 +14,9 @@ def rewrite(filename):
     for line in contents.split('\r\n'):
         match = deadkey_line.match(line)
         if match:
-            output.append(line + " (" + unicodedata.name(line[-1]) + ")")
+            dead_char = unichr(int(match.group(1), 16))
+            output.append(match.group(0)
+                    + " (" + unicodedata.name(dead_char) + ")")
         else:
             output.append(line)
     with open(filename, 'wb') as outfile:
